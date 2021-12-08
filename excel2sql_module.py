5e68ca1f75be9563ff0b67b50e967ced0f9bfcf6
dohemm <89053962+dohemm@users.noreply.github.com>
2021-12-08 17:34:57 +0900

    Update excel2sql_module.py

--- excel2sql_module.py
+++ excel2sql_module.py
@@ -1,119 +1,160 @@
-#쿼리 한번에 작성 후 execute
+#쿼리 한나씩 execute
 
 #라이브러리 호출
 
 import psycopg2
 import time
+import pandas as pd
+import numpy as np
 
 #필요한 값 입력 받기
-host = input('input "Host"')
-dbname = input('input "DBname"')
-user = input('input "User"')
-password = input('input "Password"')
-port = input('input "Port"')
-table_name = input('input "Table name"')
+host = input('input "Host": ')
+dbname = input('input "DBname": ')
+user = input('input "User": ')
+password = input('input "Password": ')
+port = input('input "Port": ')
 
 #pgadmin과 연동
+server_suc = 0
+file_suc = 0
+
 try:
     conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
     print("connected to postgreSQL")
     print("-----------------")
-    cur = conn.cursor()
+    server_suc = 1
+    #엑셀파일 선택(xlsx, xls, csv)
     
-
 except:
-    print("not connected")
+    
+    
+    print("서버 접속 오류(입력값을 다시 확인하세요)")
+    conn.close()
+    print("postgreSQL과의 연결이 종료되었습니다.")
+    
+if server_suc== 1:
+    
+    filename = input("같은 폴더 내에 있는 파일명을 입력하세요(확장자명까지): ")
     print("-----------------")
-    
-########################################################################################################################
-########################################################################################################################   
 
+    try:
+        df = pd.read_excel(filename) 
+    except: 
+        pass
 
-#엑셀파일 선택(xlsx, xls, csv)
-import os
-import pandas as pd
-import numpy as np
-from tkinter import filedialog
-from tkinter import messagebox
-
-#files 변수에 선택 파일 경로 넣기
-files = filedialog.askopenfilenames(initialdir="/",\
-                 title = "파일을 선택 해 주세요",\
-                    filetypes = (("*.xlsx","*xlsx"),("*.xls","*xls"),("*.csv","*csv")))
-
-
-
-#파일 선택 안했을 때 메세지 출력
-if files == '':
-    messagebox.showwarning("경고", "파일을 추가 하세요")    
-
-    
-files = list(files)
-fail_list = files[:]
-success_list = []
-print("file list: ") 
-print(files)    #files 리스트 값 출력
-print("-----------------")
-
-
-
-
-    
-########################################################################################################################
-########################################################################################################################
-start = time.time() #시간 측정 시작
-    
-#dir_path에 파일경로 하나씩 넣어서 읽기
-
-
-for dir_path in files:                                             
-    df = pd.read_excel(dir_path)   
-    df_numpy = df.to_numpy()
-    print(dir_path.split("/")[-1], "file is running....")
-    
-    #SQL문 실행    
-    
-    sqlString_head = "INSERT INTO " + table_name + " VALUES "
-    sqlString_tail = ""
-    
-    file_name = dir_path.split("/")[-1]
-    
-
-    
-    for idx in range(len(df)):
-        sqlString_tail += "(%s, %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s, '%s'), " %tuple(df_numpy[idx])
-        
-    sqlString_tail = sqlString_tail[:-2]
-    sqlString = sqlString_head + sqlString_tail
+    try:
+        df = pd.read_csv(filename, encoding='cp949') 
+    except: 
+        pass
     
     try:
-        cur = conn.cursor()
-        cur.execute(sqlString)
-        conn.commit()
-        print(file_name, "file's Commit is done:") 
-        print("Duration of time: ", time.time() - start, "Seconds")#시간 측정 종료
-        print("-----------------")
-        
-        fail_list.remove(dir_path)
-        success_list.append(dir_path)
-
+        df_numpy = df.to_numpy()      #넘파이 행렬로 변환하여 속도 향상
+        del df #안 쓰는 데이터 프레임 삭제하여 메모리 확보
+        file_suc = 1
+        print("table shape: ", df_numpy.shape)
+        print("파일이 정상적으로 로드되었습니다. ")
+    
     except:
-        print(file_name, "file cannot Execute in SQL")
+        print("파일 오류(확장자, 파일 존재유무 확인)")
+        conn.close()
         print("-----------------")
-        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port) #재접속
+        print("Program end")
         
         
+if file_suc == 1:
+
+    table_name = input('input "Table name": ')
+    
+    print("------------------------------")
+    print("Host: ", host)
+    print("DBname: ", dbname)
+    print("User: ", user)
+    print("Table: ", table_name)
+    print("------------------------------")
+    print("위의 테이블로", filename, "데이터를 전송합니다. 맞습니까?")
+    
+    check = input("y/n로 입력")
+    
+    if check == "y" :
+        start = time.time() #시간 측정 시작
 
 
-# print("SQL commit is done")
-# print("-----------------")
-conn.close()
-print("SQL connection ended")
-print("-----------------")
-print("Success File List:")
-print(success_list)
-print("-----------------")
-print("Fail File List:")
-print(fail_list)
-print("-----------------")
-print("Total Duration of time: ", time.time() - start, "Seconds")#시간 측정 종료
+        #SQL문 실행    
+
+        sqlString = "INSERT INTO " + table_name + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+
+        fail_row = []
+        fail_company_id = []
+
+        cur = conn.cursor()
+
+        for idx in range(len(df_numpy)):
+
+            try:
+                cur.execute(sqlString, tuple(df_numpy[idx]))
+
+
+
+            except :
+
+                fail_row.append(idx)
+                fail_company_id.append(df_numpy[idx][0])
+
+                conn.rollback()
+                break
+
+        if len(fail_row) ==0 :
+            conn.commit()
+            print("------------------------------")
+
+            print("Commit is done.")
+
+            print("Total Duration of time: ", time.time() - start, "Seconds")#시간 측정 종료
+
+
+
+        else:
+            print("------------------------------")
+
+            print("[오류발견]commit을 진행하지 않습니다!")
+            print("오류 행(0부터 시작): ", fail_row)
+            print("오류 회사 코드: ", fail_company_id)
+
+
+
+
+
+        # print("SQL commit is done")
+        # print("-----------------")
+        conn.close()
+        print("postgreSQL과의 연결이 종료되었습니다.")#시간 측정 종료
+    
+    elif check == "n" :
+        conn.close()
+        print("------------------------------")
+
+        print("postgreSQL과의 연결이 종료되었습니다.")
+        
+    else:
+        conn.close()
+        print("y, n 이외의 문자가 입력되었습니다.")
+        print("postgreSQL과의 연결이 종료되었습니다.")
+
+
+
+
+
+
+
+
+
+    ########################################################################################################################
+    ########################################################################################################################
+
+
+
+
+
+
+
+
