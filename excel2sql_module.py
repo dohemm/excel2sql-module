a8cd1cc9fb9e6877a4a9c4ba3812ee7cd2f53212
dohemm <89053962+dohemm@users.noreply.github.com>
2021-12-22 14:25:02 +0900

    Update excel2sql_module.py

--- excel2sql_module.py
+++ excel2sql_module.py
@@ -1,20 +1,20 @@
-#쿼리 하나씩 execute
+# 쿼리 한나씩 execute
 
-#라이브러리 호출
-
+# 라이브러리 호출
+# import openpyxl        #  openpyxl 라이브러리 필요시 설치 및 import!
 import psycopg2
 import time
 import pandas as pd
 import numpy as np
 
-#필요한 값 입력 받기
+# 필요한 값 입력 받기
 host = input('input "Host": ')
 dbname = input('input "DBname": ')
 user = input('input "User": ')
 password = input('input "Password": ')
 port = input('input "Port": ')
 
-#pgadmin과 연동
+# pgadmin과 연동
 server_suc = 0
 file_suc = 0
 
@@ -23,48 +23,48 @@ try:
     print("connected to postgreSQL")
     print("-----------------")
     server_suc = 1
-    #엑셀파일 선택(xlsx, xls, csv)
-    
+
 except:
-    
-    
+
     print("서버 접속 오류(입력값을 다시 확인하세요)")
     conn.close()
     print("postgreSQL과의 연결이 종료되었습니다.")
-    
-if server_suc== 1:
-    
+
+if server_suc == 1:
+
     filename = input("같은 폴더 내에 있는 파일명을 입력하세요(확장자명까지): ")
     print("-----------------")
 
+    # 엑셀파일 선택(xlsx, xls, csv, 등..)
     try:
-        df = pd.read_excel(filename) 
-    except: 
+        df = pd.read_excel(filename)
+        df = df.fillna('null')
+    except:
         pass
 
     try:
-        df = pd.read_csv(filename, encoding='cp949') 
-    except: 
+        df = pd.read_csv(filename, encoding='cp949')
+        df = df.fillna('null')
+    except:
         pass
-    
+
     try:
-        df_numpy = df.to_numpy()      #넘파이 행렬로 변환하여 속도 향상
-        del df #안 쓰는 데이터 프레임 삭제하여 메모리 확보
+        df_numpy = df.to_numpy()  # 넘파이 행렬로 변환하여 속도 향상
+        del df  # 안 쓰는 데이터 프레임 삭제하여 메모리 확보
         file_suc = 1
         print("table shape: ", df_numpy.shape)
         print("파일이 정상적으로 로드되었습니다. ")
-    
+
     except:
         print("파일 오류(확장자, 파일 존재유무 확인)")
         conn.close()
         print("-----------------")
         print("Program end")
-        
-        
+
 if file_suc == 1:
 
     table_name = input('input "Table name": ')
-    
+
     print("------------------------------")
     print("Host: ", host)
     print("DBname: ", dbname)
@@ -72,16 +72,13 @@ if file_suc == 1:
     print("Table: ", table_name)
     print("------------------------------")
     print("위의 테이블로", filename, "데이터를 전송합니다. 맞습니까?")
-    
+
     check = input("y/n로 입력")
-    
-    if check == "y" :
-        start = time.time() #시간 측정 시작
 
+    if check == "y":
+        start = time.time()  # 시간 측정 시작
 
-        #SQL문 실행    
-
-        sqlString = "INSERT INTO " + table_name + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        # SQL문 실행
 
         fail_row = []
         fail_company_id = []
@@ -91,11 +88,16 @@ if file_suc == 1:
         for idx in range(len(df_numpy)):
 
             try:
-                cur.execute(sqlString, tuple(df_numpy[idx]))
+
+                sqlString = "INSERT INTO " + table_name + " VALUES ({0}, {1}, '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', {13}, '{14}')".format(
+                    df_numpy[idx][0], df_numpy[idx][1], df_numpy[idx][2], df_numpy[idx][3], df_numpy[idx][4],
+                    df_numpy[idx][5], df_numpy[idx][6], df_numpy[idx][7], df_numpy[idx][8], df_numpy[idx][9],
+                    df_numpy[idx][10], df_numpy[idx][11], df_numpy[idx][12], df_numpy[idx][13], df_numpy[idx][14])
+                cur.execute(sqlString)
 
 
 
-            except :
+            except:
 
                 fail_row.append(idx)
                 fail_company_id.append(df_numpy[idx][0])
@@ -103,13 +105,13 @@ if file_suc == 1:
                 conn.rollback()
                 break
 
-        if len(fail_row) ==0 :
+        if len(fail_row) == 0:
             conn.commit()
             print("------------------------------")
 
             print("Commit is done.")
 
-            print("Total Duration of time: ", time.time() - start, "Seconds")#시간 측정 종료
+            print("Total Duration of time: ", time.time() - start, "Seconds")  # 시간 측정 종료
 
 
 
@@ -121,40 +123,19 @@ if file_suc == 1:
             print("오류 회사 코드: ", fail_company_id)
 
 
-
-
-
-        # print("SQL commit is done")
-        # print("-----------------")
         conn.close()
-        print("postgreSQL과의 연결이 종료되었습니다.")#시간 측정 종료
-    
-    elif check == "n" :
+        print("postgreSQL과의 연결이 종료되었습니다.")  # 시간 측정 종료
+
+    elif check == "n":
         conn.close()
         print("------------------------------")
-
+        print("파일을 전송하지 않습니다")
         print("postgreSQL과의 연결이 종료되었습니다.")
-        
+
     else:
         conn.close()
         print("y, n 이외의 문자가 입력되었습니다.")
         print("postgreSQL과의 연결이 종료되었습니다.")
 
-
-
-
-
-
-
-
-
     ########################################################################################################################
     ########################################################################################################################
-
-
-
-
-
-
-
-
